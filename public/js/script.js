0972cd3bed23d31c6c61801d272db17fac012aa4
Niv Suroghon <nsuroghon@gmail.com>
2021-01-27 22:09:04 -0500

    front page ny times api

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,36 @@
-var mykey = config.MY_KEY;
-var secretkey = config.SECRET_KEY;
+let mykey = config.MY_KEY;
+let secretkey = config.SECRET_KEY;
 
-fetch('https:api.nytimes.com/svc/topstories/v2/home.json?api-key=' + mykey,
-)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log('ERROR'))
+let getStories = fetch('https:api.nytimes.com/svc/topstories/v2/home.json?api-key=' + mykey,)
+    .then(response => response.json())
+    .then(data => {
+    
+    //variable to hold content we need from api
+        let timesApi = data.results;
+    // loop through api to recieve the first 10 trending posts
+    
+        // console.log(document.querySelector("#target"))
+        for (let i = 0; i < 10; i++) {
+        console.log(timesApi[i]);
+
+        const post =`
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img alt="" class="img-fluid rounded-circle" src="${timesApi[i].multimedia[0].url}">
+                        </div>
+                    </div>
+
+                    <div class="col-md-9">
+                            <h4 id="trendingTitle">${timesApi[i].title}</h4>
+                            <p id="trendingAuthor">${timesApi[i].byline}</p>
+                        </div>
+                    </div>`
+            document.querySelector("#target").innerHTML += post 
+    }});
+
+
+// Step 1: Get generate 10 random #s from 0-53 (top trending stories)
+// Step 2: Perform step one only every 24 hours
+// Step 3: Of the 10 numbers chosen get data needed for front page
+// Ex:
+
