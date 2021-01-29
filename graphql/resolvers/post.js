7af5a6fba86dd4833a9ad249aba6c053f2521ac9
Dazrin Tioseco <xdazee@gmail.com>
2021-01-29 14:52:00 -0800

    Got delete post functionality working!

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,5 +1,5 @@
 const { Post } = require('../../models');
-const checkAuth = require("../context/index");
+const { verifyToken } = require("../context/index");
 
 const { AuthenticationError } = require('apollo-server-express');
 
@@ -7,6 +7,7 @@ module.exports = {
 
     // Resolver function to handle mutations and queries related to Post model
   Mutation: {
+    // Create Post Mutation
     async createPost(_, { title, body, }, { user = null }) {
       if (!user) {
         throw new AuthenticationError('You must login to create a post');
@@ -17,7 +18,29 @@ module.exports = {
         body,
       });
     },
-  },
+
+
+  // Delete Post Mutation
+   async deletePost(_, { postId }, { user = null }){
+
+    // Ensures you must be logged in to delete a post
+    if (!user) {
+      throw new AuthenticationError('You must login to delete your post');
+    }
+      // Finds a specific post with a matching id in the database
+        const post = Post.findByPk(postId);
+
+        // Checks if the user's id matches the id of the user who created the post to be deleted
+        if(user.userId === post.userId){
+          Post.destroy({
+            where: { id: postId }
+           })
+          return 'Post deleted successfully';
+        } else {
+            throw new AuthenticationError('Action not allowed');
+        }
+      }
+    },
 
   // Get all posts in the database
   Query: {
